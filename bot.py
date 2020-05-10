9b0ebeed5c27b8e5a6c5176207e515907116d632
root <root@ivanov.cf>
2020-05-10 11:56:26 -0400

    task

--- bot.py
+++ bot.py
@@ -2,23 +2,15 @@ import telebot
 from telebot import types
 
 import config
+import utils
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.message_handler(commands=['enter', 'start'])
 def main(message):
-    mid = message.chat.id
-    keyboard = types.InlineKeyboardMarkup()
-    btn1 = types.InlineKeyboardButton(text='Да', callback_data='yes')
-    btn2 = types.InlineKeyboardButton(text='Пойдем курить', callback_data='cig')
-    btn3 = types.InlineKeyboardButton(text='Куда положить печенье?', callback_data='cks')
-    btn4 = types.InlineKeyboardButton(text='Как принимать матпомощь?', callback_data='mhp')
-    keyboard.add(btn1)
-    keyboard.add(btn2)
-    keyboard.add(btn3)
-    keyboard.add(btn4)
-    bot.send_message(mid, 'У тебя что-то срочное?', reply_markup=keyboard)
+    keyboard = utils.get_Vadim_keyb()
+    bot.send_message(message.chat.id, utils.Vadim_q, reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=["text"])
@@ -26,17 +18,13 @@ def echo(message):
     bot.send_message(message.chat.id, message.text)
 
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback_inline(call):
-    if call.message:
-        if call.data == "yes":
-            bot.send_message(chat_id=call.message.chat.id, text='Борода')
-        elif call.data == 'cig':
-            bot.send_message(chat_id=call.message.chat.id, text='Курить вредно')
-        elif call.data == 'cks':
-            bot.send_message(chat_id=call.message.chat.id, text='Мне в рот')
-        elif call.data == 'mhp':
-            bot.send_message(chat_id=call.message.chat.id, text='Денег нет, но вы держитесь')
+@bot.callback_query_handler(func=lambda call: call.message and call.message.text == utils.Vadim_q)
+def Vadim_callback(call):
+    mid = call.message.chat.id
+    bot.edit_message_text(chat_id = mid,
+            message_id = call.message.message_id,
+            text = call.message.text)
+    bot.send_message(chat_id=mid, text=utils.Vadim_ans[call.data])
 
 
 if __name__ == '__main__':
