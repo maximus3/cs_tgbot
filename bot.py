22c7ea74b22a419818ef0b63f5054df7d3be6149
maximus3 <max2000.gym5cheb@yandex.ru>
2020-05-10 13:20:25 +0300

    Hi

---
+++ bot.py
@@ -0,0 +1,12 @@
+import telebot
+
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.infinity_polling()
